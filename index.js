61e070b132c51c2fb8fea38555a4e56f3b5202ca
Ian Copp <copp.ian@gmail.com>
2017-09-18 14:24:11 -0700

    Fix bug in entry point handling

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+const fs = require('fs')
 const path = require('path')
 const { getLoader, getBabelLoader } = require('react-app-rewired')
 
+/**
+ * @param {object} config
+ * @param {object} config.resolve
+ * @param {string[]} config.resolve.extensions
+ * @param {object} config.module
+ * @param {any[]} config.module.rules
+ * @param {string[]} config.entry
+ */
 function rewireTypescript(config, env, typescriptLoaderOptions = {}) {
+  // Monkey patch react-scripts paths to use just `src` instead of
+  // `src/index.js` specifically. Hopefully this can get removed at some point.
+  // @see https://github.com/facebookincubator/create-react-app/issues/3052
+  let paths = require('react-scripts/config/paths')
+  if (paths) {
+    paths.appIndexJs = path.resolve(fs.realpathSync(process.cwd()), 'src')
+  }
+
+  // Add Typescript files to automatic file resolution for Webpack.
   config.resolve.extensions = (config.resolve.extensions || []).concat([
     '.web.ts',
     '.ts',
     '.tsx'
   ])
 
-  const typescriptExtension = /\.tsx?$/
-
+  // Add Typescript as an exception to the file-loader defaults.
   const fileLoader = getLoader(
     config.module.rules,
     rule =>
@@ -17,19 +34,26 @@ function rewireTypescript(config, env, typescriptLoaderOptions = {}) {
       typeof rule.loader === 'string' &&
       rule.loader.endsWith(`file-loader${path.sep}index.js`)
   )
-  fileLoader.exclude.push(typescriptExtension)
+  fileLoader.exclude.push(/\.tsx?$/)
 
+  // Add Typescript to the loader rules, using a copy of the existing Babel
+  // configs.
   const babelLoader = getBabelLoader(config.module.rules)
-
   const typescriptRules = {
-    test: typescriptExtension,
+    test: /\.tsx?$/,
     use: [
-      babelLoader,
+      { loader: babelLoader.loader, options: babelLoader.options },
       { loader: 'ts-loader', options: typescriptLoaderOptions }
     ]
   }
-
   config.module.rules.push(typescriptRules)
+
+  // Change the hardcoded `index.js` to just `index`, so that it will resolve as
+  // whichever file is available. The use of `fs` is to handle things like
+  // symlinks.
+  config.entry = config.entry
+    .slice(0, config.entry.length - 1)
+    .concat([path.resolve(fs.realpathSync(process.cwd()), 'src/index')])
 
   return config
 }
