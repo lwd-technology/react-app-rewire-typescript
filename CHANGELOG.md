2d10bf936161ee0701bcb0c6f013e82fbe3b4521
Ian Copp <copp.ian@gmail.com>
2017-10-31 16:54:53 -0700

    Update to 2.0.0 with new ts-loader version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+# 2.0.0
+
+- Update to `ts-loader` 3.1.1 and update devDependencies
+
 # 1.0.5
 
 - Account for configuration change in `react-scripts` 1.0.15
--- index.js
+++ index.js
@@ -3,6 +3,46 @@ const path = require('path')
 const { getLoader, getBabelLoader } = require('react-app-rewired')
 
 /**
+ * @param {Object} rule
+ * @return {Array}
+ */
+const ruleChildren = rule =>
+  rule.use || rule.oneOf || (Array.isArray(rule.loader) && rule.loader) || []
+
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
+  let result
+  const rules = Array.isArray(rulesSource)
+    ? rulesSource
+    : ruleChildren(rulesSource)
+  rules.some(
+    (rule, index) =>
+      (result = ruleMatcher(rule)
+        ? { index, rules }
+        : findIndexAndRules(ruleChildren(rule), ruleMatcher))
+  )
+  return result
+}
+
+/**
+ * Given a rule, return if it uses a specific loader.
+ */
+const createLoaderMatcher = loader => rule =>
+  rule.loader && rule.loader.indexOf(`/${loader}/`) !== -1
+
+/**
+ * Get the existing file-loader config.
+ */
+const fileLoaderMatcher = createLoaderMatcher('file-loader')
+
+/**
+ * Add one rule before another in the list of rules.
+ */
+const addBeforeRule = (rulesSource, ruleMatcher, value) => {
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher)
+  rules.splice(index, 0, value)
+}
+
+/**
  * @param {object} config
  * @param {object} config.resolve
  * @param {string[]} config.resolve.extensions
@@ -19,6 +59,13 @@ function rewireTypescript(config, env, typescriptLoaderOptions = {}) {
     paths.appIndexJs = path.resolve(fs.realpathSync(process.cwd()), 'src')
   }
 
+  // Change the hardcoded `index.js` to just `index`, so that it will resolve as
+  // whichever file is available. The use of `fs` is to handle things like
+  // symlinks.
+  config.entry = config.entry
+    .slice(0, config.entry.length - 1)
+    .concat([path.resolve(fs.realpathSync(process.cwd()), 'src/index')])
+
   // Add Typescript files to automatic file resolution for Webpack.
   config.resolve.extensions = (config.resolve.extensions || []).concat([
     '.web.ts',
@@ -26,18 +73,7 @@ function rewireTypescript(config, env, typescriptLoaderOptions = {}) {
     '.tsx'
   ])
 
-  // Add Typescript as an exception to the file-loader defaults.
-  const fileLoader = getLoader(
-    config.module.rules,
-    rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.indexOf(`${path.sep}file-loader${path.sep}`) !== -1
-  )
-  fileLoader.exclude.push(/\.tsx?$/)
-
-  // Add Typescript to the loader rules, using a copy of the existing Babel
-  // configs.
+  // Set up a Typescript rule.
   const babelLoader = getBabelLoader(config.module.rules)
   const typescriptRules = {
     test: /\.tsx?$/,
@@ -46,14 +82,9 @@ function rewireTypescript(config, env, typescriptLoaderOptions = {}) {
       { loader: 'ts-loader', options: typescriptLoaderOptions }
     ]
   }
-  config.module.rules.push(typescriptRules)
 
-  // Change the hardcoded `index.js` to just `index`, so that it will resolve as
-  // whichever file is available. The use of `fs` is to handle things like
-  // symlinks.
-  config.entry = config.entry
-    .slice(0, config.entry.length - 1)
-    .concat([path.resolve(fs.realpathSync(process.cwd()), 'src/index')])
+  // Add the Typescript rule before the file-loader rule.
+  addBeforeRule(config.module.rules, fileLoaderMatcher, typescriptRules)
 
   return config
 }
