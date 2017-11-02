66235e288bdb67bcac1516f255d99f02f341c6b6
Ian Copp <copp.ian@gmail.com>
2017-11-01 18:24:32 -0700

    Remove an extra import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const { getLoader, getBabelLoader } = require('react-app-rewired')
+const { getBabelLoader } = require('react-app-rewired')
 
 /**
  * @param {Object} rule
